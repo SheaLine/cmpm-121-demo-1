529f5128dbb27bbda1780267107ba6a186bba1ef
SheaLine <sline@ucsc.edu>
2024-10-04 18:50:27 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,15 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-let num_clicks = 0;
+let num_clicks : number = 0;
 const button = document.createElement("button");
 button.innerHTML = "🎅";
 button.onclick = () => {
-    num_clicks++;
-    button.innerHTML = `🎅 ${num_clicks}`;
-}
+  num_clicks++;
+  counter.innerHTML = `${num_clicks} Santas`;
+};
 app.append(button);
+
+const counter = document.createElement("div");
+counter.innerHTML = `${num_clicks} Santas`;
+app.append(counter);
